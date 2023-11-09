46d82d50681586b31196bbe198f355839a431079
Eze-cordoba <ezecordoba4@gmail.com>
2023-11-09 20:09:30 -0300

    comit -quitando algunos comentarios innecesarios 2

--- src/main/java/com/gestionalumnosescuela1/gestionAlumnos/security/config/SecurityConfiguration.java
+++ src/main/java/com/gestionalumnosescuela1/gestionAlumnos/security/config/SecurityConfiguration.java
@@ -70,20 +70,21 @@ public class SecurityConfiguration{
         .requestMatchers("/utn/eliminarAlumno/{id}").hasAnyRole(ADMIN.name(), MANAGER.name())
         .requestMatchers("/utn/editarAlumno/{id}").hasAnyRole(ADMIN.name(), MANAGER.name())
         .requestMatchers( "/utn/alumnos/{id}").hasAnyRole(ADMIN.name(), MANAGER.name())
- 
-     
-        .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
-        .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
-        .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
-        .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+
+            /*
+
+       .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+       .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+       .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+       .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
 
 
-       /* .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+     .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
 
-        .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
-        .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
-        .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
-        .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())*/
+       .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+       .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+       .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+       .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())*/
 
 
         .anyRequest()
